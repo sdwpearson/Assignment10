93239b22a85c9f64aa03cba9d7d76aae028b3d62
sdwpearson <sdwpearson@gmail.com>
2019-04-06 21:15:30 -0400

    [PHY1610] forgot int designator for process_rank

--- walkring_timestep.cc
+++ walkring_timestep.cc
@@ -31,7 +31,7 @@
 //  have been given a chance to move on position to the left or two
 //  the right).
 //
-void walkring_timestep(rarray<int,1>& walkerpositions, int N, double prob, process_rank)
+void walkring_timestep(rarray<int,1>& walkerpositions, int N, double prob, int process_rank)
 {
     int Z = walkerpositions.size();
 
